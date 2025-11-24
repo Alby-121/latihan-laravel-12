8f07539a66bf6767d9834c706a119bd9c524df7c
alby <alby@gmail.com>
2025-11-24 19:34:39 +0700

    membuat rooting

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,29 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-    $data = ['laptop', 'Laptop', 'Keyboard'];
-    return view('home', compact('data'));
+    return view('home');
 });
 
 Route::get('/about', function () {
-    return view('about');
+    $data = ['laptop', 'Laptop', 'Keyboard'];
+    return view('about', compact('data'));
+});
+// Basic POST route 
+Route::get('/contact', function () {
+    return 'Contact from submitted!';
+});
+
+// Method 1: Array 
+// Route::get('/user/{name}', function ($nama) {
+//     return view('user', ['nama' => $nama]);
+// });
+
+// Method 2: with() method
+Route::get('/user/{name}', function ($name){
+    return view('user')->with('name', $name);
+});
+
+// Folder structure: resources/view/admin/users.blade.php
+Route::get('/admin/user', function () {
+    return view('admin.users');
 });
