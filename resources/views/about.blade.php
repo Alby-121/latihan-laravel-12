9a2ebd1f744c5ad7e7e9bd868606eadc55f3b7c9
alby <alby@gmail.com>
2025-11-24 19:34:20 +0700

    membuat about dan user

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>About</title>
 </head>
 <body>
-    <h1>ini halaman ABOUT</h1>
+    <h1>ini halaman HOME</h1>
+    <h2>Daftar Produk</h2>
+    <ul>
+        @foreach ($data as $d)
+            <li>{{ $d }}</li>           
+        @endforeach
+    </ul>
 </body>
 </html>
---
+++ resources/views/user.blade.php
@@ -0,0 +1,2 @@
+{{-- resources/views/user.blade.php --}}
+<h1>Hello, {{ $name }}!</h1>
